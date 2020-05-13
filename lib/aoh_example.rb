38d86a2504e8ec8f2344037e581439bb9e816560
zara ahmadi <zatokey@gmail.com>
2020-05-13 14:58:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,6 +19,7 @@ def literal_aoh
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   { name:  "Pat Monteseian", occupation: "Staff" },
   { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 end
 
