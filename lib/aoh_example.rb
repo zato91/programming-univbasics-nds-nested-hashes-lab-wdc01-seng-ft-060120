046bbadbe2868b759a97439086f7469995149bc1
zara ahmadi <zatokey@gmail.com>
2020-05-13 14:56:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
